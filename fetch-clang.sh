66aaf386102749528d882fd492462fa267716966
Bob Steagall <bob.steagall.cpp@gmail.com>
2020-03-26 10:10:53 -0400

    Fetch from old and new download sites

--- fetch-clang.sh
+++ fetch-clang.sh
@@ -17,36 +17,62 @@ source ./clang-build-vars.sh
 mkdir -p ./tarballs
 cd ./tarballs
 
-CLANG_URL=https://github.com/llvm/llvm-project/releases/download/llvmorg-$CLANG_VERSION
+OLD_CLANG_URL=https://releases.llvm.org/$CLANG_VERSION
+NEW_CLANG_URL=https://github.com/llvm/llvm-project/releases/download/llvmorg-$CLANG_VERSION
 
 echo "Checking for required tarballs... "
 
 if [ ! -e $LLVM_TARBALL ]
 then
     echo "Downloading $LLVM_TARBALL... "
-    wget $CLANG_URL/$LLVM_TARBALL
+    wget $NEW_CLANG_URL/$LLVM_TARBALL
+
+    if [ $? -ne 0 ]
+    then
+        wget $OLD_CLANG_URL/$LLVM_TARBALL
+    fi
 fi
 
 if [ ! -e $CFE_TARBALL ]
 then
     echo "Downloading $CFE_TARBALL... "
-    wget $CLANG_URL/$CFE_TARBALL
+    wget $NEW_CLANG_URL/$CFE_TARBALL
+
+    if [ $? -ne 0 ]
+    then
+        wget $OLD_CLANG_URL/$CFE_TARBALL
+    fi
 fi
 
 if [ ! -e $CRT_TARBALL ]
 then
     echo "Downloading $CRT_TARBALL... "
-    wget $CLANG_URL/$CRT_TARBALL
+    wget $NEW_CLANG_URL/$CRT_TARBALL
+
+    if [ $? -ne 0 ]
+    then
+        wget $OLD_CLANG_URL/$CRT_TARBALL
+    fi
 fi
 
 if [ ! -e $CTX_TARBALL ]
 then
     echo "Downloading $CTX_TARBALL... "
-    wget $CLANG_URL/$CTX_TARBALL
+    wget $NEW_CLANG_URL/$CTX_TARBALL
+
+    if [ $? -ne 0 ]
+    then
+        wget $OLD_CLANG_URL/$CTX_TARBALL
+    fi
 fi
 
 if [ ! -e $LIB_TARBALL ]
 then
     echo "Downloading $LIB_TARBALL... "
-    wget $CLANG_URL/$LIB_TARBALL
+    wget $NEW_CLANG_URL/$LIB_TARBALL
+
+    if [ $? -ne 0 ]
+    then
+        wget $OLD_CLANG_URL/$LIB_TARBALL
+    fi
 fi
